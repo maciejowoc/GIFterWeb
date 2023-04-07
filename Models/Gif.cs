f0cac62beb48efaaeb078b87c3d09e82cdfe6bfc
maciek <maciej.owoc8.gmail.com>
2023-04-07 03:04:49 +0200

    Added Gif model

---
+++ Models/Gif.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GIFterWeb.Models
+{
+    public class Gif
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Image { get; set; }
+        [Required]
+        public string[] Tags { get; set; }
+        public Gif()
+        {
+            
+        }
+    }
+}
