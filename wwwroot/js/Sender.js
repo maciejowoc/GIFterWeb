2ec5c7b3d67f055a9385224d4d18833df08545db
Maciej Owoc <miausztain@gmail.com>
2023-04-26 01:21:55 +0200

    [fix] Added visual styles to MyGifs
    Gifs will be shown in card form
    Fixed tags input so the last comma won't be included in database

--- wwwroot/js/Sender.js
+++ wwwroot/js/Sender.js
@@ -8,6 +8,7 @@ btn.addEventListener('click', () => {
     var condition = true;
     document.querySelectorAll('.tag').forEach(element => element.value != '' ? Tags += element.value + ',' : condition = false);
     if (condition) {
+        Tags = Tags.substring(0, Tags.length - 1);
         tagInput.value = Tags;
         form.submit();
     }
