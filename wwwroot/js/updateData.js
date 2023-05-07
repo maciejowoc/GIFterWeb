a524c0fc88beef914a0e2f2aa50d227787031f41
Maciej Owoc <miausztain@gmail.com>
2023-05-07 12:08:14 +0200

    [fix] Fixed edit data
    I had to do it different way but now it's fully working

--- wwwroot/js/updateData.js
+++ wwwroot/js/updateData.js
@@ -1,41 +1,16 @@
 ﻿const modal = document.querySelector('#alterModal');
-const inputs = document.querySelector('#tagsField');
+const inputs = document.querySelector('#tags');
 const btn = document.querySelector('#submit');
 const form = document.querySelector('#EditFormula');
 const tagsConnected = document.querySelector('#senderTags');
 
-function ShowInterface(e) { 
-    modal.showModal();
-    let tags = e.parentElement.previousElementSibling.children;   
-    let tagsRefined = [];
-    debugger;
-    for (let i = 0; i < tags.length; i++) {
-        tagsRefined.push(tags[i].innerHTML);
-    }
-    
-    tagsRefined.forEach((element,index) => {
-        const tagSpan = document.createElement('li');
-        const tagInput = document.createElement("input");
-        tagInput.setAttribute('type', 'text');
-        tagInput.setAttribute('class', 'tag');
-        tagInput.setAttribute('value', element);
-        const tagButton = document.createElement("span");
-        tagButton.setAttribute('class', 'btnRemoveTag');
-        tagButton.innerHTML = '-';
-        tagButton.addEventListener('click', removeTag);
-        tagSpan.appendChild(tagInput);
-        if (index != 0) { tagSpan.appendChild(tagButton); }
-        document.querySelector('#tagsField').appendChild(tagSpan);
-    });
-    document.querySelector('#gifId').value = e.parentElement.children[0].innerHTML;
-}
-function cancelModal() {
-    while (inputs.firstChild) {
-        inputs.removeChild(inputs.firstChild);
-    }
-    modal.close();
-}
+//  Removing delete tag button from first tag in edit gif page
+inputs.firstElementChild.lastElementChild.remove();
 
+//  Adding event listeners to earlier created buttons
+document.querySelectorAll('.btnRemoveTag').forEach(obj => obj.addEventListener('click', removeTag));
+
+//  Add tag method from generator since it works identically
 document.querySelector('#addTag').addEventListener('click', () => {
     let tags = document.querySelectorAll('.tag');
     if (tags.length < 5) {
@@ -49,24 +24,30 @@ document.querySelector('#addTag').addEventListener('click', () => {
         tagButton.addEventListener('click', removeTag);
         tagSpan.appendChild(tagInput);
         tagSpan.appendChild(tagButton);
-        document.querySelector('#tagsField').appendChild(tagSpan);
+        inputs.appendChild(tagSpan);
     }
 })
 
+//  Removes input tag field with button itself
 function removeTag() {
     this.parentNode.remove();
 }
 
+//  Process data to edit similar like in generator
 function send() {
     var Tags = '';
     var condition = true;
     document.querySelectorAll('.tag').forEach(element => element.value != '' ? Tags += element.value + ',' : condition = false);
     if (condition) {
         Tags = Tags.substring(0, Tags.length - 1);
-        debugger;
         tagsConnected.value = Tags;
     }
     else {
         alert('Please fill every field');
     }
 }
+
+//  Return to MyGifs page
+document.querySelector('#cancel').addEventListener('click', () => {
+    history.back();
+})
